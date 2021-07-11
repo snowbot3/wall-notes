8ba4e1ba9e89fa932e30af4ea33263655ef69cbb
snowbot3 <snowbot3@gmail.com>
2021-07-10 21:49:19 -0500

    add live reload while developing

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const jsonServer = require('json-server');
+const livereload = require('livereload');
+const lrserver = livereload.createServer();
+const path = require('path');
+const source = path.join(__dirname, 'public');
+lrserver.watch(source);
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
